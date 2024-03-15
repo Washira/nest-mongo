89b0b6e944ad40df5d2c737d042dcf02dbf22b80
washira <washira.suyajai@gmail.com>
2024-03-15 22:59:53 +0700

    ✨ (products.controller.ts): add support for retrieving single product by ID and creating a new product in the ProductsController ♻️ (products.controller.ts): refactor getAllProducts and addProduct methods to use ProductsService for better separation of concerns and code reusability

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,16 +1,28 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
   @Get()
   getAllProducts() {
-    return 'This will return all products';
+    return this.productsService.getProducts();
+  }
+
+  @Get(':id')
+  getProduct(@Param() param: { id: string }) {
+    return this.productsService.getSingleProduct(param.id);
   }
 
   @Post()
   addProduct(
     @Body() body: { name: string; description: string; price: number },
   ) {
-    return body;
+    return this.productsService.createProduct(
+      body.name,
+      body.description,
+      body.price,
+    );
   }
 }
