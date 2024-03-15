9733e9fbcf896b039f548b5cca10db826ad79a8b
washira <washira.suyajai@gmail.com>
2024-03-15 22:48:34 +0700

    ✨ (products.controller.ts): Add GET and POST endpoints to ProductsController to handle retrieving all products and adding a new product ✨ (products.service.ts): Implement methods in ProductsService to get all products, get a single product by ID, and create a new product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  @Get()
+  getAllProducts() {
+    return 'This will return all products';
+  }
+
+  @Post()
+  addProduct(
+    @Body() body: { name: string; description: string; price: number },
+  ) {
+    return body;
+  }
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  private products = [
+    {
+      id: '123',
+      name: 'product name',
+      description: 'product description',
+      price: 123,
+    },
+    {
+      id: '456',
+      name: 'product name 2',
+      description: 'product description 2',
+      price: 456,
+    },
+  ];
+
+  getProducts() {
+    return this.products;
+  }
+
+  getSingleProduct(productId: string) {
+    return this.products.find((product) => product.id === productId);
+  }
+
+  createProduct(name: string, description: string, price: number) {
+    const newProduct = {
+      id: new Date().toString(),
+      name,
+      description,
+      price,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+}
