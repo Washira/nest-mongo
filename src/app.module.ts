e230b11a617284efc0a33b542db26ea4e9145175
washira <washira.suyajai@gmail.com>
2024-05-15 21:00:06 +0700

    ⬆️ (package.json): upgrade dependencies for @nestjs/passport and passport-local 📝 (app.module.ts): add AuthModule to the imports array for better organization and separation of concerns ✨ (auth): introduce AuthController, AuthService, and related modules for handling authentication logic
    🔧 (order.schema.ts, create-product.dto.ts, update-product.dto.ts, product.entity.ts, products.controller.spec.ts, products.controller.ts, products.module.ts, products.service.ts, product.schema.ts): Remove unnecessary files related to product management as they are no longer needed in the project.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { OrdersModule } from './orders/orders.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env',
     }), // load .env file
     MongooseModule.forRoot('mongodb://localhost:27017', {
       user: process.env.DATABASE_USER,
       pass: process.env.DATABASE_PASSWORD,
       dbName: process.env.DATABASE_NAME,
     }), // connect to the database
-    ProductsModule,
-    OrdersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
